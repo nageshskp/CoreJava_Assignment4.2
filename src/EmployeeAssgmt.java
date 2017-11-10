5bfbc9c671171eb662abad5db64ea2768eca2a6b
Nagesh S <Nagesh S@Nagesh-GT7CM2Q9>
2017-11-10 11:18:36 +0530

    CoreJava Assignment 4.2

---
+++ src/EmployeeAssgmt.java
@@ -0,0 +1,16 @@
+//public class EmployeeAssgmt {  
+	class Faculty
+	{  
+	float salary=30000;  
+	}  
+	public class EmployeeAssgmt extends Faculty
+	{ 
+	float bonous=2000;
+	public static void main(String args[])
+	{
+		EmployeeAssgmt obj=new EmployeeAssgmt(); 
+	System.out.println("Salary is:"+obj.salary);  
+	System.out.println("Bonous is:"+obj.bonous);  
+	}  
+	} 
+//}  
---
+++ src/EmployeeAssgmtHASA.java
@@ -0,0 +1,13 @@
+class Employee
+{  
+float salary=30000;  
+}  
+class EmployeeAssgmtHASA extends Employee
+{ 
+float bonous=2000;
+public static void main(String args[])
+{
+	EmployeeAssgmtHASA obj=new EmployeeAssgmtHASA();
+System.out.println("Salary is:"+obj.salary);  
+}  
+}  
